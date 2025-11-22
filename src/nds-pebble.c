940917bff588dc1a634b1efe19be9554f4c04e7c
Arenovas <54878864+Arenovas@users.noreply.github.com>
2025-11-21 21:04:18 -0800

    Big update for more dynamic elements

--- src/nds-pebble.c
+++ src/nds-pebble.c
@@ -1,54 +1,39 @@
+#include <pebble.h>
 #include "nds-pebble.h"
 
-#include "pebble.h"
+static Window *s_main_window;
+static Layer *s_bgsq_layer;
+static Layer *s_topbar_layer;
+static Layer *s_clock_layer;
+static GPath *s_tick_paths[NUM_CLOCK_TICKS];
 
-static Window *s_window;
-//static Layer *s_simple_bg_layer, *s_date_layer, *s_hands_layer;
 static Layer *s_date_layer, *s_hands_layer;
 static TextLayer *s_time_label, *s_date_label;
-
-//static GPath *s_tick_paths[NUM_CLOCK_TICKS];
-//static GPath *s_minute_arrow, *s_hour_arrow;
 static char s_num_buffer[6], s_day_buffer[6];
 // Declare globally
 static GFont s_time_font;
-static BitmapLayer *s_background_layer;
-static GBitmap *s_background_bitmap;
 static BitmapLayer *s_batt_layer;
 static GBitmap *s_batt_bitmap;
 static int s_battery_level;
 static Layer *s_battery_layer;
-static BitmapLayer *s_background_layer, *s_bt_icon_layer;
-static GBitmap *s_background_bitmap, *s_bt_icon_bitmap;
-static GBitmap *s_background_bitmap, *s_bt_icon_bitmap_on;
-
-/*static void bg_update_proc(Layer *layer, GContext *ctx)
-{
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	for (int i = 0; i < NUM_CLOCK_TICKS; ++i) 
-	{
-		const int x_offset = 0;
-		const int y_offset = 0;
-		gpath_move_to(s_tick_paths[i], GPoint(x_offset, y_offset));
-		gpath_draw_filled(ctx, s_tick_paths[i]);
-	}
-}*/
+static BitmapLayer *s_bt_icon_layer;
+static GBitmap *s_bt_icon_bitmap;
+static GBitmap *s_bt_icon_bitmap_on;
+static BitmapLayer *s_topsep_layer;
+static GBitmap *s_topsep_bitmap;
 
 static void hands_update_proc(Layer *layer, GContext *ctx)
 {
 	GRect bounds = layer_get_bounds(layer);
 	GPoint center = grect_center_point(&bounds);
 	center.y = center.y;
-
 	const int16_t second_hand_length = (bounds.size.w / 2) - 12;
 	const int16_t minute_hand_length = (bounds.size.w / 2) - 14;
 	const int16_t hour_hand_length = (bounds.size.w / 2) - 23;
 
 	time_t now = time(NULL);
 	struct tm *t = localtime(&now);
-	
+
 	//Draw the second hand
 	int32_t second_angle = TRIG_MAX_ANGLE * t->tm_sec / 60;
 	GPoint second_hand = {
@@ -76,20 +61,11 @@ static void hands_update_proc(Layer *layer, GContext *ctx)
 	graphics_draw_line(ctx, second_hand, center);
 
 	// minute/hour hand
-	//graphics_context_set_fill_color(ctx, GColorBlack);
 	graphics_context_set_stroke_color(ctx, GColorDarkGray);
 	graphics_draw_line(ctx, minute_hand, center);
 
 	graphics_context_set_stroke_color(ctx, GColorBlack);
 	graphics_draw_line(ctx, hour_hand, center);
-
-	//gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
-	//gpath_draw_filled(ctx, s_minute_arrow);
-	//gpath_draw_outline(ctx, s_minute_arrow);
-
-	//gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
-	//gpath_draw_filled(ctx, s_hour_arrow);
-	//gpath_draw_outline(ctx, s_hour_arrow);
 
 	// dot in the middle
 	graphics_context_set_fill_color(ctx, GColorBlack);
@@ -110,7 +86,7 @@ static void date_update_proc(Layer *layer, GContext *ctx)
 
 static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed)
 {
-	layer_mark_dirty(window_get_root_layer(s_window));
+	layer_mark_dirty(window_get_root_layer(s_main_window));
 }
 
 static void battery_callback(BatteryChargeState state)
@@ -159,33 +135,182 @@ static void bluetooth_callback(bool connected)
 	}
 }
 
-static void window_load(Window *window)
+static void bgsq_proc(Layer *layer, GContext *ctx){
+	GRect bounds = layer_get_bounds(layer);
+	graphics_context_set_fill_color(ctx, GColorWhite);
+	graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+	graphics_context_set_stroke_color(ctx, GColorBlack);
+	#if PBL_DISPLAY_HEIGHT == 228
+	//Draw Top Bar Line
+	graphics_draw_line(ctx, GPoint(0, 0), GPoint(200, 0));
+	for(int y = 0; y < 10; y++)
+	{
+		int posy = 1 + (24 * y);
+		for(int x = 0; x < 10; x++)
+		{
+			int posx = -20 + (24 * x);
+			graphics_draw_rect(ctx, GRect(posx, posy, 24, 24));
+			if((x < 2 || x > 7) || (y < 1 || y > 6))
+			{
+				for(int lines = 0; lines < 8; lines++)
+				{
+					GPoint p1 = GPoint(3 + posx, 3 + (lines * 3) + posy);
+					GPoint p2 = GPoint(20 + posx, 3 + (lines * 3) + posy);
+					graphics_draw_line(ctx, p1, p2);
+				}
+			}
+		}
+	}
+	//Draw Inner and Outer Clock Borders
+	graphics_draw_rect(ctx, GRect(26, 23, 148, 148));
+	graphics_draw_rect(ctx, GRect(25, 22, 150, 150));
+	#elif PBL_DISPLAY_HEIGHT == 180
+	for(int y = 0; y < 10; y++)
+	{
+		int posy = 0 + (16 * y);
+		for(int x = 0; x < 12; x++)
+		{
+			int posx = -7 + (16 * x);
+			graphics_draw_rect(ctx, GRect(posx, posy, 17, 17));
+			if((x < 3 || x > 8) || (y < 1 || y > 6))
+			{
+				for(int lines = 0; lines < 8; lines++)
+				{
+					GPoint p1 = GPoint(2 + posx, 2 + (lines * 2) + posy);
+					GPoint p2 = GPoint(14 + posx, 2 + (lines * 2) + posy);
+					graphics_draw_line(ctx, p1, p2);
+				}
+			}
+		}
+	}
+	//Draw Clock Border
+	graphics_draw_rect(ctx, GRect(40, 15, 99, 99));
+	#else
+	for(int y = 0; y < 10; y++)
+	{
+		int posy = 0 + (16 * y);
+		for(int x = 0; x < 10; x++)
+		{
+			int posx = -8 + (16 * x);
+			graphics_draw_rect(ctx, GRect(posx, posy, 17, 17));
+			if((x < 2 || x > 7) || (y < 1 || y > 6))
+			{
+				for(int lines = 0; lines < 8; lines++)
+				{
+					GPoint p1 = GPoint(2 + posx, 2 + (lines * 2) + posy);
+					GPoint p2 = GPoint(14 + posx, 2 + (lines * 2) + posy);
+					graphics_draw_line(ctx, p1, p2);
+				}
+			}
+		}
+	}
+	//Draw Clock Border
+	graphics_draw_rect(ctx, GRect(23, 15, 99, 99));
+	#endif
+
+}
+
+static void topbar_proc(Layer *layer, GContext *ctx)
 {
+	GRect bounds = layer_get_bounds(layer);
+	graphics_context_set_fill_color(ctx, GColorWhite);
+	graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+
+	// Create GBitmap
+	s_topsep_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TOPSEP_ICON);
+
+	// Create BitmapLayer to display the GBitmap
+	s_topsep_layer = bitmap_layer_create(GRect(0, 0, 1, 14));
+
+	bitmap_layer_set_compositing_mode(s_topsep_layer, GCompOpSet);
+	bitmap_layer_set_bitmap(s_topsep_layer, s_topsep_bitmap);
+	layer_add_child(layer, bitmap_layer_get_layer(s_topsep_layer));
+}
+
+static void clock_proc(Layer *layer, GContext *ctx)
+{
+	//GRect bounds = layer_get_bounds(layer);
+	//bounds.size.h -= 58;
+	//bounds.size.w -= 49;
+	//bounds.origin.y += 2;
+	//bounds.origin.x += 25;
+	graphics_context_set_fill_color(ctx, GColorBlack);
+	/*//1
+	graphics_fill_rect(ctx, GRect(70, 10, 4, 4), 0, GCornerNone);
+	//2
+	graphics_fill_rect(ctx, GRect(86, 26, 4, 4), 0, GCornerNone);
+	//4
+	graphics_fill_rect(ctx, GRect(86, 70, 4, 4), 0, GCornerNone);
+	//5
+	graphics_fill_rect(ctx, GRect(70, 86, 4, 4), 0, GCornerNone);
+	//7
+	graphics_fill_rect(ctx, GRect(26, 86, 4, 4), 0, GCornerNone);
+	//8
+	graphics_fill_rect(ctx, GRect(10, 70, 4, 4), 0, GCornerNone);
+	//10
+	graphics_fill_rect(ctx, GRect(10, 26, 4, 4), 0, GCornerNone);
+	//11
+	graphics_fill_rect(ctx, GRect(26, 10, 4, 4), 0, GCornerNone);
+	//11
+	graphics_fill_rect(ctx, GRect(47, 47, 5, 5), 0, GCornerNone);*/
+	for (int i = 0; i < NUM_CLOCK_TICKS; ++i)
+	{
+		const int x_offset = 0;
+		const int y_offset = 0;
+		gpath_move_to(s_tick_paths[i], GPoint(x_offset, y_offset));
+		gpath_draw_filled(ctx, s_tick_paths[i]);
+	}
+}
+
+static void main_window_load(Window *window) {
+	#if PBL_DISPLAY_HEIGHT == 228
+	int offset = 22;
+	#elif PBL_DISPLAY_HEIGHT == 180
+	int offset = 25;
+	#else
+	int offset = 15;
+	#endif
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
+
+	GRect layer_bounds = bounds;
+	layer_bounds.size.h -= offset;
+	layer_bounds.origin.y += offset;
+	s_bgsq_layer = layer_create(layer_bounds);
+
+	layer_set_update_proc(s_bgsq_layer, bgsq_proc);
+	layer_add_child(window_get_root_layer(window), s_bgsq_layer);
+
+	//s_clock_layer = layer_create(GRect(23, 15, 99, 99));
+	//layer_set_update_proc(s_clock_layer, clock_proc);
+	//layer_add_child(s_bgsq_layer, s_clock_layer);
+
+	#if PBL_DISPLAY_HEIGHT == 228
+	s_clock_layer = layer_create(GRect(25, 44, 150, 150));
+	#elif PBL_DISPLAY_HEIGHT == 180
+	s_clock_layer = layer_create(GRect(40, 40, 99, 99));
+	#else
+	s_clock_layer = layer_create(GRect(23, 30, 99, 99));
+	#endif
+	layer_set_update_proc(s_clock_layer, clock_proc);
+	layer_add_child(window_get_root_layer(window), s_clock_layer);
+
+	layer_bounds.size.h = offset;
+	layer_bounds.origin.y -= offset;
+	s_topbar_layer = layer_create(layer_bounds);
+
+	layer_set_update_proc(s_topbar_layer, topbar_proc);
+	layer_add_child(window_get_root_layer(window), s_topbar_layer);
 
 	// Create GFont
 	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_NINTENDO_DS_BIOS_15));
 
-
-	// Create GBitmap
-	s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-
-	// Create BitmapLayer to display the GBitmap
-	s_background_layer = bitmap_layer_create(bounds);
-
-	// Set the bitmap onto the layer and add to the window
-	bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-	layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
-
-
 	s_date_layer = layer_create(bounds);
 	layer_set_update_proc(s_date_layer, date_update_proc);
-	layer_add_child(window_layer, s_date_layer);
+	layer_add_child(s_topbar_layer, s_date_layer);
 
 	s_time_label = text_layer_create(GRect(50, -3, 29, 15));
 	text_layer_set_text(s_time_label, s_day_buffer);
-	//text_layer_set_background_color(s_time_label, GColorWhite);
 	text_layer_set_text_color(s_time_label, GColorBlack);
 	text_layer_set_font(s_time_label, s_time_font);
 
@@ -193,16 +318,15 @@ static void window_load(Window *window)
 
 	s_date_label = text_layer_create(GRect(83, -3, 27, 15));
 	text_layer_set_text(s_date_label, s_num_buffer);
-	//text_layer_set_background_color(s_date_label, GColorWhite);
 	text_layer_set_text_color(s_date_label, GColorBlack);
 	text_layer_set_font(s_date_label, s_time_font);
 
 	layer_add_child(s_date_layer, text_layer_get_layer(s_date_label));
+	GRect boundsClock = layer_get_bounds(s_clock_layer);
 
-	//s_hands_layer = layer_create(bounds);
-	s_hands_layer = layer_create(GRect(25, 32, 95, 95));
+	s_hands_layer = layer_create(boundsClock);
 	layer_set_update_proc(s_hands_layer, hands_update_proc);
-	layer_add_child(window_layer, s_hands_layer);
+	layer_add_child(s_clock_layer, s_hands_layer);
 
 	// Create battery meter Layer
 	s_battery_layer = layer_create(GRect(130, 4, 12, 7));
@@ -222,15 +346,12 @@ static void window_load(Window *window)
 	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_icon_layer));
 }
 
-static void window_unload(Window *window)
-{
+static void main_window_unload(Window *window) {
+	layer_destroy(s_bgsq_layer);
+	layer_destroy(s_topbar_layer);
+	layer_destroy(s_clock_layer);
+
 	layer_destroy(s_date_layer);
-
-	// Destroy BitmapLayer
-	bitmap_layer_destroy(s_background_layer);
-
-	// Destroy GBitmap
-	gbitmap_destroy(s_background_bitmap);
 
 	// Destroy BitmapLayer
 	bitmap_layer_destroy(s_batt_layer);
@@ -255,39 +376,40 @@ static void window_unload(Window *window)
 	gbitmap_destroy(s_bt_icon_bitmap_on);
 }
 
-static void init()
-{
-	s_window = window_create();
-	window_set_window_handlers(s_window, (WindowHandlers) {
-		.load = window_load,
-		.unload = window_unload,
+static void init() {
+	// Create main Window element and assign to pointer
+	s_main_window = window_create();
+
+	// Set handlers to manage the elements inside the Window
+	window_set_window_handlers(s_main_window, (WindowHandlers) {
+		.load = main_window_load,
+		.unload = main_window_unload
 	});
-	window_stack_push(s_window, true);
+
+	// Show the Window on the watch, with animated=true
+	window_stack_push(s_main_window, true);
+
+	#if PBL_DISPLAY_HEIGHT == 228
+	for (int i = 0; i < NUM_CLOCK_TICKS; ++i)
+	{
+		s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS_EMERY[i]);
+	}
+	#else
+	for (int i = 0; i < NUM_CLOCK_TICKS; ++i)
+	{
+		s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS[i]);
+	}
+	#endif
 
 	s_day_buffer[0] = '\0';
 	s_num_buffer[0] = '\0';
-
-	// init hand paths
-	//s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
-	//s_hour_arrow = gpath_create(&HOUR_HAND_POINTS);
-
-	//Layer *window_layer = window_get_root_layer(s_window);
-	//GRect bounds = layer_get_bounds(window_layer);
-	//GPoint center = grect_center_point(&bounds);
-	//gpath_move_to(s_minute_arrow, center);
-	//gpath_move_to(s_hour_arrow, center);
-
-	/*for (int i = 0; i < NUM_CLOCK_TICKS; ++i) 
-	{
-		s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS[i]);
-	}*/
 
 	tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
 	// Ensure battery level is displayed from the start
 	battery_callback(battery_state_service_peek());
 
 	// Register for Bluetooth connection updates
-	connection_service_subscribe((ConnectionHandlers) 
+	connection_service_subscribe((ConnectionHandlers)
 	{
 		.pebble_app_connection_handler = bluetooth_callback
 	});
@@ -296,22 +418,17 @@ static void init()
 	bluetooth_callback(connection_service_peek_pebble_app_connection());
 }
 
-static void deinit()
-{
-	//gpath_destroy(s_minute_arrow);
-	//gpath_destroy(s_hour_arrow);
-
-	/*for (int i = 0; i < NUM_CLOCK_TICKS; ++i) 
+static void deinit() {
+	// Destroy Window
+	tick_timer_service_unsubscribe();
+	window_destroy(s_main_window);
+	for (int i = 0; i < NUM_CLOCK_TICKS; ++i)
 	{
 		gpath_destroy(s_tick_paths[i]);
-	}*/
-
-	tick_timer_service_unsubscribe();
-	window_destroy(s_window);
+	}
 }
 
-int main()
-{
+int main(void) {
 	init();
 	app_event_loop();
 	deinit();
